ff95e7249712cfd985c16a28eff551d036838179
Elise <elise.beaussart@viacesi.fr>
2022-06-19 23:12:28 +0200

    Add card in recent work

--- assets/js/main.js
+++ assets/js/main.js
@@ -121,3 +121,24 @@ function toggle1() {
 	var popup = document.getElementById('popup1');
 	popup.classList.toggle('active')
 }
+
+function toggle2() {
+	var blur = document.getElementById('blur');
+	blur.classList.toggle('active')
+	var popup = document.getElementById('popup2');
+	popup.classList.toggle('active')
+}
+
+function toggle3() {
+	var blur = document.getElementById('blur');
+	blur.classList.toggle('active')
+	var popup = document.getElementById('popup3');
+	popup.classList.toggle('active')
+}
+
+function toggle4() {
+	var blur = document.getElementById('blur');
+	blur.classList.toggle('active')
+	var popup = document.getElementById('popup4');
+	popup.classList.toggle('active')
+}
